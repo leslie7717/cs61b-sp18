3d80b087afd4698af6a8accf1ef87278c50c512d
realsing <gmy6324@gmail.com>
2022-05-13 22:37:26 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum_ = 0;
+        while (x <= 9) {
+            sum_ = sum_ + x;
+            System.out.print(sum_ + " ");
+            x = x + 1;
+
+        }
+    }
+}
